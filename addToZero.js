6735ac0e51ad46a691dacb0e94efca95b535a534
Jacob Spackman <jacobts202@gmail.com>
2023-03-21 20:00:10 -0600

    assignment complete

--- addToZero.js
+++ addToZero.js
@@ -1,11 +1,15 @@
 // Starting array
-let array = [28, 43, -12, 30, 4, 0, 12]
+let array = [28, 43, -12, 30, 4, 0, 13]
 
 // Write your solution below:
 let answer = false
 for(let i = 0; i < array.length; i++){
-    if(array[i] + array[0,1,2,3,4,5,6] === 0){
-        answer = true
+    for(let k = 0; k < array.length; k++){
+        if(i !== k){
+            if(array[i] + array[k] === 0){
+                answer = true
+            }
+        }
     }
 }
 console.log(answer);
