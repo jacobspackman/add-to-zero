cb02c8bd9c98a2d27354d4ac1b51ddd8fc3e0af1
Jacob Spackman <jacobts202@gmail.com>
2023-03-21 19:42:12 -0600

    initail commit

---
+++ addToZero.js
@@ -0,0 +1,11 @@
+// Starting array
+let array = [28, 43, -12, 30, 4, 0, 12]
+
+// Write your solution below:
+let answer = false
+for(let i = 0; i < array.length; i++){
+    if(array[i] + array[0,1,2,3,4,5,6] === 0){
+        answer = true
+    }
+}
+console.log(answer);
